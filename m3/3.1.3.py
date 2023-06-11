3f66b166a49269343cbdc4421a5bd8aefa4d87f1
serg <sergey4891@gmail.com>
2023-06-11 17:28:45 +0300

    задание 3.2

--- m3/3.1.3.py
+++
@@ -1,6 +0,0 @@
-num = int(input())
-sum = 0
-while num != 0:
-    sum += num % 10
-    num = num // 10
-print(sum)
---
+++ m3/3.2.3.py
@@ -0,0 +1,4 @@
+d ={'name1': 'id1', 'name2': 'id2', 'name3': 'id3'}
+y = {v:k for k, v in d.items()}
+print(y)
+
