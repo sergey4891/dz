8fb1916342c73d1ed6c995cfd4c74ba490ef5674
Sergey <sergey4891@gmail.com>
2023-10-21 15:53:53 +0300

    5

--- modul 4/ва.py
+++ modul 4/ва.py
@@ -1,19 +1,23 @@
-class Person:
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-    @property
-    def fullname(self):
-        return self.name + ' ' + self.surname
 
-    @fullname.setter
-    def fullname(self, fullname):
-        self.name, self.surname = fullname.split()
+class Matrix:
+    def __init__(self, data):
+        self.data = data
+        self.n = len(data)
+        self.m = len(data[0])
+    def __add__(self, other):
+        if self.m == other.m and self.n == other.n:
+            data1 = []
+            for i in range(self.n):
+                curr_data =[]
+                for j in range(self.m):
+                    curr_data.append(self.data[i][j] + other.data[i][j])
+                data1.append(curr_data)
+            return Matrix(data1)
+        else:
+            return "размер не правельный"
 
+m1 = Matrix([[1,2], [1,2]])
+m2 = Matrix([[1,0], [0,1]])
+m3 = m1 + m2
 
-person = Person('Mike', 'Pondsmith')
-
-print('Troy Baker'.isspace())
-person.fullname = 'Troy Baker'
-print(person.name)
-print(person.surname)
+print(m3.data)
--- modul 5/5.3.py
+++ modul 5/5.3.py
@@ -1,95 +1,36 @@
 import random
 
+
 class Warrior:
-    def __init__(self, name, health=100, armor_points=20, stamina_points=30):
+    def __init__(self, name, health=100):
         self.name = name
         self.health = health
-        self.armor_points = armor_points
-        self.stamina_points = stamina_points
 
     def attack(self, enemy):
-        damage = random.randint(10, 30)
-        self.stamina_points -= 10
-        if self.stamina_points < 0:
-            damage -= random.randint(0, 10)
-        if damage < 0:
-            damage = 0
-        enemy.defend(self, damage)
-
-    def defend(self, attacker, damage):
-        if self.stamina_points > 0:
-            self.stamina_points -= random.randint(0, 20)
-            self.armor_points -= random.randint(0, 10)
-            if self.armor_points < 0:
-                self.health -= damage
-            if self.health < 0:
-                self.health = 0
-            print(f"{self.name} защищается и имеет {self.health} здоровья и {self.armor_points} брони.")
-        else:
-            self.health -= damage
-            if self.health < 0:
-                self.health = 0
-            print(f"{self.name} не может больше защищаться и имеет {self.health} здоровья.")
+        damage = random.randint(10,20)
+        print(f"{self.name} атакует {enemy.name} и наносит {damage} урона.")
+        enemy.health -= damage
 
     def is_alive(self):
-        return self.health > 10
+        return self.health > 0
+
 
 def main():
-    name1 = input("Введите имя первого воина: ")
-    name2 = input("Введите имя второго воина: ")
-
-    warrior1 = Warrior(name1)
-    warrior2 = Warrior(name2)
+    warrior1 = Warrior("Воин Змей")
+    warrior2 = Warrior("Воин Илья")
 
     while warrior1.is_alive() and warrior2.is_alive():
-        if random.choice([True, False]):
-            warrior1.attack(warrior2)
-        if random.choice([True, False]):
-            warrior2.attack(warrior1)
+        attacker = random.choice([warrior1, warrior2])
+        enemy = warrior2 if attacker == warrior1 else warrior1
+        attacker.attack(enemy)
+        print(f"{enemy.name} имеет {enemy.health} здоровья.")
 
     if warrior1.is_alive():
         print(f"{warrior1.name} одержал победу!")
     else:
         print(f"{warrior2.name} одержал победу!")
 
+
 if __name__ == "__main__":
     main()
-
-    import random
-
-
-    class Warrior:
-        def __init__(self, name, health=100):
-            self.name = name
-            self.health = health
-
-        def attack(self, enemy):
-            damage = 20
-            print(f"{self.name} атакует {enemy.name} и наносит {damage} урона.")
-            enemy.health -= damage
-
-        def is_alive(self):
-            return self.health > 0
-
-
-    def main():
-        warrior1 = Warrior("Воин 1")
-        warrior2 = Warrior("Воин 2")
-
-        while warrior1.is_alive() and warrior2.is_alive():
-            attacker = random.choice([warrior1, warrior2])
-            enemy = warrior2 if attacker == warrior1 else warrior1
-            attacker.attack(enemy)
-            print(f"{enemy.name} имеет {enemy.health} здоровья.")
-
-        if warrior1.is_alive():
-            print(f"{warrior1.name} одержал победу!")
-        else:
-            print(f"{warrior2.name} одержал победу!")
-
-
-    if __name__ == "__main__":
-        main()
-
-
 
