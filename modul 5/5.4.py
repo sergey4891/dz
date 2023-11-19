6db4b96875c2620c66dc3bb29c12d7754b8177aa
Sergey <sergey4891@gmail.com>
2023-11-19 22:36:59 +0300

    Модуль 5.4

--- modul 5/5.4.py
+++ modul 5/5.4.py
@@ -2,21 +2,25 @@ import json
 
 class Model:
     def save(self, filename):
-        data = {}
-        for attr, value in self.__dict__.items():
-            if not callable(value):
-                data[attr] = value
+        # Создаем словарь со всеми атрибутами объекта
+        obj_dict = {}
+        for field_name in self.__dict__:
+            obj_dict[field_name] = getattr(self, field_name)
 
+        # Записываем словарь в файл JSON
         with open(filename, 'w') as json_file:
-            json.dump(data, json_file)
+            json.dump(obj_dict, json_file, indent=4)
 
-# Пример использования для класса Post (наследование от Model):
-class Post(Model):
-    def __init__(self, title, content, date_posted, author):
-        self.title = title
-        self.content = content
-        self.date_posted = date_posted
-        self.author = author
+
+class Post(models.Model, Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+post = Post.objects.get(pk=1)  # Получаем объект Post из базы данных
+post.save('post.json')  # Сохраняем его в файл 'post.json'
