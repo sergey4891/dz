6be80ef8925352bb9b0c775600995eff0ac75521
Sergey <sergey4891@gmail.com>
2023-11-20 21:51:49 +0300

    Модуль 5.4

--- modul 5/5.4.py
+++ modul 5/5.4.py
@@ -1,26 +1,27 @@
 import json
 
-class Model:
-    def save(self, filename):
-        # Создаем словарь со всеми атрибутами объекта
-        obj_dict = {}
-        for field_name in self.__dict__:
-            obj_dict[field_name] = getattr(self, field_name)
 
-        # Записываем словарь в файл JSON
-        with open(filename, 'w') as json_file:
-            json.dump(obj_dict, json_file, indent=4)
-
-
-class Post(models.Model, Model):
+class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
 
+    def save_to_json(self, file_path):
+        post_data = {
+            'title': self.title,
+            'content': self.content,
+            'author': self.author,
+        }
 
-post = Post.objects.get(pk=1)  # Получаем объект Post из базы данных
-post.save('post.json')  # Сохраняем его в файл 'post.json'
+        with open(file_path, 'w') as json_file:
+            json.dump(post_data, json_file)
+            
+            
+post = Post(title='Заголовок', content='Содержание', author='пользователь'))
+
+
+post.save_to_json('post.json')
